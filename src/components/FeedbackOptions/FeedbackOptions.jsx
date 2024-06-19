7e470d4c4084375569819788b63024b7ddab3efc
HappieDo08 <aquinogerardoXIII@gmail.com>
2024-06-19 19:24:29 +0800

    initial commit

---
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types';
+import style from '../FeedbackOptions/FeedbackOptions.module.css';
+
+export const FeedbackOptions = ({ options, onLeaveFeedback }) => (
+    <div className={style.feedback__container}>
+        {Object.keys(options).map(option => (
+            <button
+                key={option}
+                type="button"
+                name={option}
+                onClick={onLeaveFeedback}
+                className={style.feedback__btn}
+            >
+            </button>
+        ))}
+    </div>
+);
+
+FeedbackOptions.propTypes = {
+    options: PropTypes.object.isRequired,
+    onLeaveFeedback: PropTypes.func.isRequired,
+}
